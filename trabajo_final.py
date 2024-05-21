c7c1b43c2f6d5f148fa76f4393147169bbf7ee0d
josenunez270594 <158522198+josenunez270594@users.noreply.github.com>
2024-05-21 16:50:46 -0500

    Create trabajo_final.py

---
+++ trabajo_final.py
@@ -0,0 +1,14 @@
+""" Para trabajo final realizare una papleleria la cual tendra como objetivo abordar todos los temas vistos en clase
+tales como, listas, diccionarios, funciones, matrizes etc...
+para la realizacion de este mismo tomare lo siguiente:
+es una papeleria tendra lo siguiente:
+nombre: PAPELERIA BUENA VISTA
+se encargara:   -REGISTRAR PRODUCTOS 
+                -DILIGENCIAR VENTAS 
+                -MOSTRAR PRODUCTOS REGISTRADOS
+                -MOSTRAR PRODUCTOS VENDIDOS
+                -DESCUENTOS
+                -REGISTRO DE EMPLEADOS
+                -MOSTRAR REGISTRO DE EMPLEADOS
+                """
+
